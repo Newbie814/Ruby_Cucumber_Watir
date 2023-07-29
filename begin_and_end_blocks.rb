465c8973b82e9abf6e3284f087576268d6c4774a
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-07-29 15:02:25 -0400

    BEGIN/END blocks

---
+++ begin_and_end_blocks.rb
@@ -0,0 +1,15 @@
+END {
+  puts "this is ending code"
+  puts "often, this is for close browser code"
+}
+
+
+puts "this is my test case code"
+
+
+BEGIN {
+  puts "this is starting code"
+}
+
+puts "This is after the begin block"
+
