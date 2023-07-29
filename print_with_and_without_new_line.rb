0cf0381b82af7842229274790ca858dede418cab
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-07-29 14:55:51 -0400

    line breaks

---
+++ print_with_and_without_new_line.rb
@@ -0,0 +1,11 @@
+# print "My name is Matthew and I love my family"
+
+# print "My name is Matthew,\n and I love my family"
+
+puts "My name is Matthew and I love my family"
+
+puts "My name is Matthew,\n and I love my family"
+
+=begin
+multiline comment
+=end
